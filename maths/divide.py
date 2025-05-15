b8b32fe0ee64ef7f2f2c55c3f9adb74d565e81af
lungelo01 <69298406+lungelo01@users.noreply.github.com>
2025-05-15 11:54:38 +0200

    first commit

---
+++ maths/divide.py
@@ -0,0 +1,13 @@
+def divide_nums(num1, num2):
+    """
+    Divide two numbers and return the result.
+
+    :param int num1: The first number
+    :param int num2: The second number
+
+    :returns: quotient
+
+    :raises ZeroDivisionError: If b is zero
+    """
+    answer = num1 / num2
+    return answer
